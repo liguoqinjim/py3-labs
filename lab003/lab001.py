7869938da3a34cc7a9b499b5ebdb3c886bf5d759
liguoqinjim <liguoqinjim23@gmail.com>
2018-10-11 14:24:32 +0800

    update config.json

--- lab003/lab001.py
+++ lab003/lab001.py
@@ -1,10 +1,15 @@
 # coding:utf8
 
 from aip import AipSpeech
+import json
+
+f = open("data/config.json", "rt")
+data = json.load(f)
+f.close()
 
 """ 你的 APPID AK SK """
-APP_ID = '1'
-API_KEY = '2'
-SECRET_KEY = '3'
+APP_ID = data["APP_ID"]
+API_KEY = data["API_KEY"]
+SECRET_KEY = data["SECRET_KEY"]
 
 client = AipSpeech(APP_ID, API_KEY, SECRET_KEY)
