719b680243c30005c86dc922bcafcfc0a3722192
liguoqinjim <liguoqinjim23@gmail.com>
2022-04-01 14:28:01 +0800

    feat($): update

--- lab038/lab001.py
+++ lab038/lab001.py
@@ -7,6 +7,15 @@ def demo():
     # logger.add(sys.stdout, level="INFO", colorize=True, format="{time} {level} {message}" )
     logger.add(sys.stdout, colorize=True, format="<green>{time:YYYY-MM-DD HH:mm:ss}</green>| <level>{message}</level>")
 
+    logger.remove()
+    # logger.add(sys.stdout, colorize=True, format="<green>{time:YYYY-MM-DD HH:mm:ss}</green> <red>|</red> <level>{level: <2}</level> <red>|</red> <cyan>{function}</cyan> <red>-</red> <level>{message}</level>")
+    logger.add(sys.stdout, colorize=True,
+               format="<green>{time:YYYY-MM-DD HH:mm:ss}</green> <red>|</red> <level>{level: <6}</level> <red>|</red> <level>{message}</level>")
+
+    # logger.add(sys.stdout, colorize=True, format="<green>{time:YYYY-MM-DD HH:mm:ss.}</green> <red>|</red> "
+    #                                              "<level>{level: <8}</level> <red>|</red> "
+    #                                              "<cyan>{name}</cyan>:<cyan>{function}</cyan>:<cyan>{line}</cyan> - <level>{message}</level>", )
+
     logger.debug("Debug")
     logger.info("Info")
     logger.error("Error")
