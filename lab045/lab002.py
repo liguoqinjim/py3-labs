b6cb7d628cfa45f24d3ec91f11d27f4b0b44b804
liguoqinjim <liguoqinjim23@gmail.com>
2022-05-20 13:55:48 +0800

    feat($): update

---
+++ lab045/lab002.py
@@ -0,0 +1,19 @@
+import os.path
+
+
+def lab001():
+    """
+
+    :return:
+    """
+    command = "python /Users/li/Workspace/py3-labs/lab045/lab001.py"
+    os.system(command)
+
+
+def run():
+    lab001()
+    pass
+
+
+if __name__ == '__main__':
+    run()
