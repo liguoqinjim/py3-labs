999dd178294fcebe29bc1daf5644118107c17356
liguoqinjim <>
2022-12-03 12:13:38 +0800

    feat($): update

---
+++ lab012/lab004.py
@@ -0,0 +1,20 @@
+import time
+import datetime
+
+# unix样例
+# 1670038783 <-> 2022-12-03 11:39:43
+
+# 获得当前时间戳01
+print(int(time.time()))
+
+# 获得当前时间戳02
+now = datetime.datetime.now()
+print(int(time.mktime(now.timetuple())))
+
+# unix->datetime
+d1 = datetime.datetime.fromtimestamp(1670038783)
+print(d1)
+
+# datetime->str
+s1 = d1.strftime("%Y-%m-%d")
+print(s1)
