c411e80ebdde64cfd10231d2be5226b7d2dd4130
liguoqinjim <>
2023-03-26 23:52:34 +0800

    feat($): update

--- lab050/lab001.py
+++ lab050/lab001.py
@@ -1,7 +1,7 @@
 import re
 
 prog = re.compile('\d{2}')  # 正则对象
-print(prog.search('12abc'))
+print(prog.search('12abc56edf'))
 # 通过调用group()方法得到匹配的字符串,如果字符串没有匹配，则返回None。
 print(prog.search('12abc').group())
 
