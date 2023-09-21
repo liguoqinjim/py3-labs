0b87e407e452bfe9e6a3333015515226ffc970b9
liguoqinjim <liguoqinjim23@gmail.com>
2023-09-21 17:19:40 +0800

    feat($): update

--- lab068/lab001.py
+++ lab068/lab001.py
@@ -27,9 +27,36 @@ def lab002() -> None:
     print(json.dumps(info, ensure_ascii=False))
 
 
+def lab003():
+    """
+    credentials = Credential(sessdata=sessdata, bili_jct=bili_jct, ac_time_value=ac_time_value)
+    """
+    from bilibili_api import Credential, sync
+    import sys
+
+    # 读取参数
+    sessdata = sys.argv[1]
+    bili_jct = sys.argv[2]
+    ac_time_value = sys.argv[3]
+    buvid3 = sys.argv[4]
+    dedeuserid = sys.argv[5]
+    credential = Credential(sessdata=sessdata, bili_jct=bili_jct, ac_time_value=ac_time_value, buvid3=buvid3, dedeuserid=dedeuserid)
+
+    # 检查 Credential 是否需要刷新
+    print(sync(credential.chcek_refresh()))
+
+    # 刷新 Credential
+    sync(credential.refresh())
+
+    # v = video.Video(bvid="BV1Ab411k7Qm", credential=credential)
+
+    # 调用player_info
+    # info = sync(v.get_player_info())
+    # print(info)
+
+
 if __name__ == '__main__':
     # asyncio.get_event_loop().run_until_complete(lab001())
 
-    lab002()
-
-
+    # lab002()
+    lab003()
